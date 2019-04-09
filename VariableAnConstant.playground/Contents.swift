d767403010fcea18ef7f7001a9185a5e528c5ba0
rathakarn <gkrgroup.co.th@gmail.com>
2019-04-09 11:12:39 +0700

    Basic Var and let

---
+++ VariableAnConstant.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+//การประกาศตัวแปรแบบ กำหนดค่าจาก Value
+var myName = 123
+
+
+
+
+//การประกาศตัวแปรโดยการกำหนดด้วยตัวเอง
+var mySurname: String = "Rathakarn"
+mySurname = "AAAAA"
+
+
+//การประกาศค่าคงที่
+
+let country: String = "Thailand"
+print("Hello World")
+print("Surname = \(mySurname)")
